b2ae4b2c68aace4719da2be4585c9f2f60d5d951
Jordan Moncharmont <jormon@gmail.com>
2015-08-21 11:55:25 -0700

    more stuff

--- db/migrate/20150820235701_add_wholesale_price_to_variants.rb
+++ db/migrate/20150820235701_add_wholesale_price_to_variants.rb
@@ -1,7 +1,7 @@
 class AddWholesalePriceToVariants < ActiveRecord::Migration
   def change
     add_column :spree_variants, \
-      :subscribed_price,
+      :wholesale_price,
       :decimal,
       precision: 8,
       scale: 2

a16043c7e57dece77190bd136dcdf08fdc1ae77b
Jordan Moncharmont <jormon@gmail.com>
2016-05-20 15:19:17 +0200

    avoid constant redeclaration

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Order.class_eval do
-  MINIMUM_ITEM_TOTAL = 100.00
+  MINIMUM_ITEM_TOTAL ||= 100.00
 
   # override this, don't let the button show up if they have less
   def minimum_spend_met?

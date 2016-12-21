21657c392407924fb283874977b3e492cd6d5895
Jordan Moncharmont <jormon@gmail.com>
2016-12-20 17:52:04 -0800

    add a wholesale querier

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,6 +1,20 @@
 Spree::Order.class_eval do
   MINIMUM_ITEM_TOTAL ||= 100.00
 
+  # returns a boolean indicating if this order contains items priced at
+  # wholesale prices.  NB: this relies on line item pricing not changing
+  # between when the order was confirmed and when it was measured as wholesale.
+  def wholesale?
+    line_items.any? do |line_item|
+      product = line_item.product
+      # a product could have multiple wholesale prices
+      [
+        product.wholesale_price,
+        product.wholesale_subscription_price
+      ].include? line_item.price
+    end
+  end
+
   # override this, don't let the button show up if they have less
   def minimum_spend_met?
     !(user.try(:wholesaler?) && item_total.to_f < MINIMUM_ITEM_TOTAL)

1352eee030c77c487c3b9b70ccafb750a1e7de7c
Jordan Moncharmont <jormon@gmail.com>
2018-05-24 11:00:00 -0700

    hound fix

--- db/migrate/20180524022908_add_wholesale_only_to_products.rb
+++ db/migrate/20180524022908_add_wholesale_only_to_products.rb
@@ -1,6 +1,6 @@
 class AddWholesaleOnlyToProducts < ActiveRecord::Migration
   def change
-    add_column :spree_products, :wholesale_only, :boolean, default: false,
-               null: false
+    add_column :spree_products, :wholesale_only, :boolean,
+      default: false, null: false
   end
 end
